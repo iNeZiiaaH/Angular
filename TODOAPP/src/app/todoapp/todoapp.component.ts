010d0d7fd05d690b3dfd511743a82c829665d224
NeZiiaaH <neziiaah@MacBook-Air-de-NeZiiaaH.local>
2023-06-15 15:17:54 +0200

    trash TODOAPP

--- TODOAPP/src/app/todoapp/todoapp.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-todoapp',
-  templateUrl: './todoapp.component.html',
-  styleUrls: ['./todoapp.component.css']
-})
-export class TodoappComponent {
-}
