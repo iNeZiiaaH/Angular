df40b4d182649a18fe1e1564871d94c470b5f055
NeZiiaaH <neziiaah@MacBook-Air-de-NeZiiaaH.local>
2023-06-19 14:12:43 +0200

    ajout d'exercice Rxjs

---
+++ RxjsTour/src/app/app.component.ts
@@ -0,0 +1,147 @@
+import { Component, SkipSelf } from '@angular/core';
+import { Observable, Subscription, catchError, concatMap, delay, filter, from, map, of, take, tap } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  firstTour() {
+    const observable = new Observable<number>((subscriber) => {
+      
+      subscriber.next(1);
+      
+      subscriber.next(2);
+      
+      subscriber.next(3);
+      
+      subscriber.next(4);
+      
+      subscriber.complete();
+    });
+    
+    observable.subscribe({
+      next(value) {
+        console.log(`valeur envoyé par l'observable : ${value}`);
+      },
+      error(err) {
+        console.error(`erreur: ${err}`);
+      },
+      complete() {
+        console.log('observable terminé');
+      },
+    })
+  }
+  secondTour() {
+    const observable = new Observable<number>((subscriber) => {
+      let value = 0;
+      const intervalId = setInterval(() => {
+        subscriber.next(++value);
+      }, 1000);
+      return () => {
+        clearInterval(intervalId);
+      };
+    });
+
+    const subscription: Subscription = observable.subscribe({
+      next(value) {
+        console.log(`valeur envoyé par l'observable : ${value}`);
+      },
+      error(err) {
+        console.error(`erreur: ${err}`);
+      },
+      complete() {
+        console.log('observable terminé');
+      },
+    });
+
+    setTimeout(() => subscription.unsubscribe(), 5000);
+  }
+  thirdTour() {
+    const observable = new Observable<number>((subscriber) => {
+      const Array = [1, 2, 3, 4];
+      
+      try {
+        for(const item of Array) {
+          subscriber.next(item);
+        } 
+        subscriber.complete(); 
+      } catch (err) {
+        subscriber.error(err);
+      }
+    });
+    
+    observable.subscribe({
+      next(value) {
+        console.log(`valeur envoyé par l'observable : ${value}`);
+      },
+      error(err) {
+        console.error(`erreur: ${err}`);
+      },
+      // complete() {
+      //   console.log('observable terminé');
+      // },
+    });
+  }
+  fourthTour() {
+    const array = [1, 2, 3, 4];
+
+    const observable = from(array);
+
+    observable.subscribe({
+      next(value) {
+        console.log(`valeur envoyé par l'observable: ${value}`);
+      },
+      // error(err) {
+      //   console.error(`erreur: ${err}`);
+      // },
+      complete() {
+        console.log(`observable terminé`)
+      },
+    });
+  }
+  fifthTour() {
+    const observable = of(1, 2, 3, 4);
+
+    observable.subscribe((value) => {
+      console.log(`valeur envoyé par l'observable: ${value}`);
+    });
+  }
+  sixthTour() {
+    const observable = of(1, 2, 3, 4).pipe(
+      map((value => value + 1))
+    );
+
+    observable.subscribe((value) => {
+      console.log(`valeur envoyé par l'observable: ${value}`);
+    });
+  }
+  seventhTour() {
+    const observable = of(1, 2, 3, 4, 5, 6, 7, 8).pipe(
+      map((value => value + 1)),
+      take(3),
+      delay(1000)
+    );
+
+    observable.subscribe((value) => {
+      console.log(`valeur envoyé par l'observable: ${value}`);
+    });
+  }
+  eighthTour() {
+    const observable = of(1, 2, 3, 4, 5, 6, 7, 8).pipe(
+      tap (value => console.log(`en cours de traitement de  ${value}`)),
+      concatMap(value => of(value).pipe(delay(1000))),
+      catchError(value => of(`erreur avec la valeur : ${value}`))
+    );
+
+    observable.subscribe((value) => {
+      console.log(`valeur envoyé par l'observable: ${value}`);
+    });
+  }
+  ninthTour() {
+  }
+  tenthTour() {
+  }
+}
